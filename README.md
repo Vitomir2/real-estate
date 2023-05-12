6bb44c05a8e7649f6bb1141b05a32f71d9cf7a47
Vitomir Pavlov <vitomirpavlov@gmail.com>
2023-05-12 23:41:45 +0300

    feature/front-end - create the front-end app and fix some issues, but still having some errors;

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # real-estate
+
 This is a simple real estate app
--- contracts/RealEstate.sol
+++ contracts/RealEstate.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+//SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/utils/Counters.sol";
@@ -13,7 +13,7 @@ contract RealEstate is ERC721URIStorage {
 
     function mint(string memory tokenURI) public returns (uint256) {
         _tokenIds.increment();
-        
+
         uint256 newItemId = _tokenIds.current();
         _mint(msg.sender, newItemId);
         _setTokenURI(newItemId, tokenURI);
